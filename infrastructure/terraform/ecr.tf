39f1f4c05dd3d7626aaf9a02c8324c5df36dd0ef
Mark Page <61083555+MarkPage2k1@users.noreply.github.com>
2023-09-13 08:43:59 +0000

    Update pipeline CI

--- infrastructure/terraform/ecr.tf
+++ infrastructure/terraform/ecr.tf
@@ -18,8 +18,7 @@ resource null_resource ecr_image {
  provisioner "local-exec" {
    command = <<EOF
            aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${local.account_id}.dkr.ecr.${var.aws_region}.amazonaws.com
-           pwd
-           ls
+           cd ../..
            docker build -t ${aws_ecr_repository.repo.repository_url}:${local.ecr_image_tag} .
            docker push ${aws_ecr_repository.repo.repository_url}:${local.ecr_image_tag}
        EOF
